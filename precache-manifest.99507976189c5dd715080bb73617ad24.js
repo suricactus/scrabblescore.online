d0f9a35f01c22ecdb9cb7a1831988aebfda80591
Anna Maniuk <anna@maniuk.nyc>
2019-06-17 20:00:38 -0400

    Production

---
+++ precache-manifest.99507976189c5dd715080bb73617ad24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9d946b5ba64155cb474",
+    "url": "/static/css/main.6db4f268.chunk.css"
+  },
+  {
+    "revision": "e9d946b5ba64155cb474",
+    "url": "/static/js/main.6ec160a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "df03c6eec8492ce03eff",
+    "url": "/static/css/2.e696dcec.chunk.css"
+  },
+  {
+    "revision": "df03c6eec8492ce03eff",
+    "url": "/static/js/2.77d31e5d.chunk.js"
+  },
+  {
+    "revision": "8f7a00da3df6cf9b01a1c5785c115678",
+    "url": "/index.html"
+  }
+];
