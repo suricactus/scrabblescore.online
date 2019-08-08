da653f6d3792367c34d1070f176f709997f7d271
Anna Maniuk <anna@maniuk.nyc>
2019-08-08 14:44:20 -0400

    Deploy

---
+++ precache-manifest.8dd60a99a02ae11617d736ed59900072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95926f92a864110dc75efca26570088",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cafb33ced913795cf4a8",
+    "url": "/static/css/2.75ee4df5.chunk.css"
+  },
+  {
+    "revision": "99283f7e446e6afd7732",
+    "url": "/static/css/main.c0b9c5a8.chunk.css"
+  },
+  {
+    "revision": "cafb33ced913795cf4a8",
+    "url": "/static/js/2.1ff35ee2.chunk.js"
+  },
+  {
+    "revision": "99283f7e446e6afd7732",
+    "url": "/static/js/main.349d10e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
