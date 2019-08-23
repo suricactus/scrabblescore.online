129d10ad9d2e7444b1d263e46a839628c3934d1b
Anna Maniuk <anna@maniuk.nyc>
2019-08-23 19:14:24 -0400

    Deploy

--- precache-manifest.07738d4856f234878cee403467ee49df.js
+++ precache-manifest.07738d4856f234878cee403467ee49df.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d59a88b0af70afef477091e1001a5da7",
+    "revision": "c13253c4115e650b76ecc8c102156d92",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.75ee4df5.chunk.css"
   },
   {
-    "revision": "66043c7957d9580c10e0",
-    "url": "/static/css/main.86aa0ff7.chunk.css"
+    "revision": "e1152bd6f85db00f29cc",
+    "url": "/static/css/main.3441adc1.chunk.css"
   },
   {
     "revision": "f517bc8fea7deaa9545f",
     "url": "/static/js/2.85eb4f8f.chunk.js"
   },
   {
-    "revision": "66043c7957d9580c10e0",
-    "url": "/static/js/main.eaf6c869.chunk.js"
+    "revision": "e1152bd6f85db00f29cc",
+    "url": "/static/js/main.503abaf9.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a66461595362f319efc4f969e8fe20e.js"
+  "/precache-manifest.07738d4856f234878cee403467ee49df.js"
 );
 
 self.addEventListener('message', (event) => {
