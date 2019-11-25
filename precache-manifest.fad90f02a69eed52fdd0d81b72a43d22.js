6780ea941b59a239e91bb522a683558726a85acc
Anna Maniuk <anna@maniuk.nyc>
2019-11-25 15:01:07 -0500

    Deploy

--- precache-manifest.fad90f02a69eed52fdd0d81b72a43d22.js
+++ precache-manifest.fad90f02a69eed52fdd0d81b72a43d22.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "062ff5cd7d5801bd14796ea7a16caff9",
+    "revision": "9083988fe9edf947ff4a9563f28bfff7",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.75ee4df5.chunk.css"
   },
   {
-    "revision": "75574da00c6f399dd624",
+    "revision": "669327f964f824c57b7b",
     "url": "/static/css/main.0a5b749b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f047efd8.chunk.js"
   },
   {
-    "revision": "75574da00c6f399dd624",
-    "url": "/static/js/main.342e4c68.chunk.js"
+    "revision": "669327f964f824c57b7b",
+    "url": "/static/js/main.37477da1.chunk.js"
   },
   {
     "revision": "fa6593056a8aff6cd31a",
