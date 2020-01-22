0308acf32ac45523dd0d4a6d3c095fc5a710c150
Ivan Ivanov <ivan.ivanov@suricactus.com>
2020-01-22 06:42:40 +0200

    Deploy

---
+++ precache-manifest.96da67a903e3fb711ad79c3b4963b432.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ea485209e4a5dafe54af4040fabd67",
+    "url": "http://suricactus.github.io/scrabblescore.online/index.html"
+  },
+  {
+    "revision": "8528fc4c79e94e2af6d4",
+    "url": "http://suricactus.github.io/scrabblescore.online/static/css/2.75ee4df5.chunk.css"
+  },
+  {
+    "revision": "d637694c01abc3f74c22",
+    "url": "http://suricactus.github.io/scrabblescore.online/static/css/main.aa3b7824.chunk.css"
+  },
+  {
+    "revision": "8528fc4c79e94e2af6d4",
+    "url": "http://suricactus.github.io/scrabblescore.online/static/js/2.18c5a3d1.chunk.js"
+  },
+  {
+    "revision": "49f11dfdee5a4d35951364942d52d77e",
+    "url": "http://suricactus.github.io/scrabblescore.online/static/js/2.18c5a3d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d637694c01abc3f74c22",
+    "url": "http://suricactus.github.io/scrabblescore.online/static/js/main.b648649d.chunk.js"
+  },
+  {
+    "revision": "deb611a885f7e6c2c206",
+    "url": "http://suricactus.github.io/scrabblescore.online/static/js/runtime-main.d6d3312e.js"
+  }
+]);
